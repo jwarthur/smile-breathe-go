b43bc08dccf8c66e1bce975da4536e29ab45b9ab
Jonathan Arthur <jonathan.arthur@intersect.org.au>
2019-03-29 17:56:39 +1100

    Working on getting this first version to work

--- smashrun_connect.py
+++ smashrun_connect.py
@@ -1,36 +1,30 @@
 #! /usr/bin/python
 
 import requests
-#from tokens import * ## loading our API details. Best to keep your credentials seperate so it's easier to manage when sharing your scripts
-
 
 print("Hello World!")
 
-## FYI - here's the search parameters we can use in our query
-# https://www.eventbrite.com/platform/api#/reference/event-search/search-events
+# Create the authentication url
 
-# First, save the base url for future use
-baseURL = "https://www.eventbriteapi.com/v3"
-
-
-# Let's set the following query parameters
-query="python"
-location = "Melbourne"
-proximity = "100km"
-q_startdate = "2019-02-19T00:00:00Z"
-q_enddate = "2019-03-01T00:00:00Z"
-
-# then we create the url with these parameters
-url = "{0}/events/search?q={1}&location.address={2}&location.within={3}&start_date.range_start={4}&start_date.range_end={5}".format(
-    baseURL, query, location, proximity, q_startdate, q_enddate)
+url = "https://secure.smashrun.com/oauth2/authenticate?client_id=client&scope=read_activity&response_type=token&state=basic&redirect_uri=http://yoursite.com/callback"
 
 ## use try/except to catch any connection or timeout errors.
+
+
 try:
-    call = requests.get(url, headers=headers) # We're GET because we are effectively getting data from Eventbrite
+    call = requests.get(url)
     print("Succesful connection", url)
+    print(call)
+
 except requests.exceptions.RequestException as err:
     '''
     This except is going to catch instances where there is a connection error.
     Other possible requests exceptions: HTTPError, ConnectionError, Timeout
     '''
     print("Ooops: Something went wrong",err)
+
+
+if call.status_code != 200: ## if not successful
+    print("Something went wrong:", call.json()['error_description'])
+else:
+    print("Successful call! You're good to go")
